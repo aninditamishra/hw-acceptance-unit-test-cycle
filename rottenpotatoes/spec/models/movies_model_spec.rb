3c29985fc77efa217eedf41e02f8416c02629a38
EC2 Default User <ec2-user@ip-172-31-27-146.us-west-2.compute.internal>
2018-10-11 02:47:55 +0000

    HW3 adding files that were missing from last push

---
+++ rottenpotatoes/spec/models/movies_model_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+require 'spec_helper'
+
+describe Movie do
+  describe '.all_ratings' do
+    it 'returns all ratings' do
+      expect(Movie.all_ratings).to match(%w(G PG PG-13 NC-17 R))
+    end
+  end
+end
